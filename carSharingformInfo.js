b24be4f13444f2e6638e966d159eafe4d11f2bb0
znewk <77445508+znewk@users.noreply.github.com>
2021-04-20 20:14:11 +0600

    Add files via upload

--- carSharingformInfo.js
+++ carSharingformInfo.js
@@ -1,10 +1,4 @@
 
-let back = () =>{
-    location.href = "index.html";
-}
-let backBtn = document.getElementById("backBtn");
-backBtn.addEventListener("click",back);
-
 let users = JSON.parse(localStorage.getItem("users"));
 console.log(users);
 
@@ -13,9 +7,9 @@ let usersBlock = $("#users");
 for(let i = 0; i < users.length; i++){
     usersBlock.append(`
         <div class = "user_card">
-            <p class = "name"><b>Name:</b>  ${users[i]['name']}</p>
-            <p class = "email"><b>Email:</b>  ${users[i]['email']}</p>
-            <p class = "password"> <b>Password:</b> ${users[i]['password']}</p>
+            <p class = "name"> Name: ${users[i]['name']}</p>
+            <p class = "email"> Email: ${users[i]['email']}</p>
+            <p class = "password"> Password: ${users[i]['password']}</p>
         </div>
     `);
 }
